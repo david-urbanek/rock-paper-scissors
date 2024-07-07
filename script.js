678567892b1dc4437610a3820f886a86c09b9f18
David <urbanek.dav@email.cz>
2024-07-07 17:28:02 +0200

    Created the UI for rock paper scissors game
    I have crated a new UI for my rock paper scissors
    scissors game

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ function playGame(){
     var humanScore = 0
     var computerScore = 0
 
+    const winner = document.querySelector("#winner");
+    const whoWin= document.querySelector("#who-win");
+
+    const humanScoreboard = document.querySelector("#human-scoreboard");
+    humanScoreboard.textContent = `${humanScore}`;
+
+    const pcScoreboard = document.querySelector("#pc-scoreboard");
+    pcScoreboard.textContent = `${computerScore}`;
+
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            humanChoice = event.target.id
+            playRound(humanChoice, getComputerChoice())
+        });
+    });
+
     function getComputerChoice(){
         var choice = Math.floor(Math.random() * 3);
 
@@ -17,61 +35,50 @@ function playGame(){
         }
     }
 
-    function getHumanChoice(){
-        return prompt("Choose between rock, paper, scissors", );
+    function checkWinner(){
+        if (computerScore == 5 || humanScore == 5) {
+            if (computerScore == 5) {
+                winner.textContent = "Pc is winner"
+                playGame()
+                }
+            else{
+                winner.textContent = "Human is winner"
+                playGame()
+                }
+            }
     }
 
     function playRound(humanChoice, computerChoice){
-        humanChoice = humanChoice.toUpperCase()
 
         if (humanChoice == computerChoice){
-            console.log("Tie");
+            whoWin.textContent = "Tie";
         }
         else if (humanChoice == "PAPER" && computerChoice == "ROCK"){
             humanScore += 1;
-            return console.log("Human won");
+            humanScoreboard.textContent = `${humanScore}`;
+            whoWin.textContent = "Human win";
+            checkWinner();
         }
         else if (humanChoice == "SCISSORS" && computerChoice == "PAPER"){
             humanScore += 1;
-            return console.log("Human won");
+            humanScoreboard.textContent = `${humanScore}`;
+            whoWin.textContent = "Human win";
+            checkWinner();
         }
         else if (humanChoice == "ROCK" && computerChoice == "SCISSORS"){
             humanScore += 1;
-            return console.log("Human won");
+            humanScoreboard.textContent = `${humanScore}`;
+            whoWin.textContent = "Human win";
+            checkWinner();
         }
         else{
             computerScore += 1;
-            return console.log("PC won");
+            pcScoreboard.textContent = `${computerScore}`;
+            whoWin.textContent = "Pc win";
+            checkWinner();
         }
     }
 
-    var humanSelection = getHumanChoice();
-    var computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-
-
-    if (computerScore > humanScore){
-        return console.log("PC won");
-    }
-    else{
-        return console.log("Human won");
-    }
 }
 
 playGame();
